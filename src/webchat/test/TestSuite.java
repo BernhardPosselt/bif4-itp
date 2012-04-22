c318f9882ab458c3a2c83a35399fb299491b4ae1
Bernhard Posselt <bernhard.posselt@gmx.at>
2012-04-22 14:13:16 +0200

    added a simple test

---
+++ src/webchat/test/TestSuite.java
@@ -0,0 +1,14 @@
+/**
+ * @author Bernhard Posselt
+ *         Date: 4/22/12
+ *         Time: 9:02 AM
+ */
+
+import example.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses( { SimpleTest.class })
+public class TestSuite {}
---
+++ src/webchat/test/example/SimpleTest.java
@@ -0,0 +1,17 @@
+package example; /**
+ * @author Bernhard Posselt
+ *         Date: 4/22/12
+ *         Time: 1:28 PM
+ */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void testSimpleTest(){
+        assertEquals(1, 1);
+    }
+
+}
