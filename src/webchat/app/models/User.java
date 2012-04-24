dadfdfa97e39d632f0df393a1a280ce3b6a7381f
Daniel Herzog <danielherzog90@gmail.com>
2012-04-24 20:27:40 +0200

    User model added

---
+++ src/webchat/app/models/User.java
@@ -0,0 +1,14 @@
+package models;
+
+
+import play.db.ebean.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Daniel
+ * Date: 24.04.12
+ * Time: 20:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class User extends Model {
+}
