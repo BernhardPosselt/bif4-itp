9a67d7594d49f9acc5f6b45a8d152bf0daaf38c0
MasterLindi <kridl.ovka1@gmx.at>
2012-05-17 13:49:29 +0200

    changed db to mem:play

--- src/webchat/conf/evolutions/default/1.sql
+++ src/webchat/conf/evolutions/default/1.sql
@@ -4,51 +4,51 @@
 # --- !Ups
 
 create table channel (
-  id                        integer auto_increment not null,
+  id                        integer not null,
   name                      varchar(255),
   topic                     varchar(255),
-  isread                    tinyint(1) default 0,
-  priv                      tinyint(1) default 0,
+  isread                    boolean,
+  priv                      boolean,
   constraint pk_channel primary key (id))
 ;
 
 create table file (
-  id                        integer auto_increment not null,
+  id                        integer not null,
   name                      varchar(255),
   type                      varchar(255),
   size                      double,
-  date                      datetime,
+  date                      timestamp,
   uid_id                    integer,
   constraint pk_file primary key (id))
 ;
 
 create table groups (
-  id                        integer auto_increment not null,
+  id                        integer not null,
   name                      varchar(255),
-  modified                  datetime,
+  modified                  timestamp,
   constraint pk_groups primary key (id))
 ;
 
 create table message (
-  id                        integer auto_increment not null,
+  id                        integer not null,
   content                   varchar(255),
   type                      varchar(255),
-  date                      datetime,
-  modified                  datetime,
+  date                      timestamp,
+  modified                  timestamp,
   user_id                   integer,
   constraint pk_message primary key (id))
 ;
 
 create table user (
-  id                        integer auto_increment not null,
+  id                        integer not null,
   username                  varchar(255),
   password                  varchar(255),
   email                     varchar(255),
   firstname                 varchar(255),
   lastname                  varchar(255),
-  online                    tinyint(1) default 0,
-  admin                     tinyint(1) default 0,
-  lastlogin                 datetime,
+  online                    boolean,
+  admin                     boolean,
+  lastlogin                 timestamp,
   constraint pk_user primary key (id))
 ;
 
@@ -82,6 +82,16 @@ create table user_groups (
   groups_id                      integer not null,
   constraint pk_user_groups primary key (user_id, groups_id))
 ;
+create sequence channel_seq;
+
+create sequence file_seq;
+
+create sequence groups_seq;
+
+create sequence message_seq;
+
+create sequence user_seq;
+
 alter table file add constraint fk_file_uid_1 foreign key (uid_id) references user (id) on delete restrict on update restrict;
 create index ix_file_uid_1 on file (uid_id);
 alter table message add constraint fk_message_user_2 foreign key (user_id) references user (id) on delete restrict on update restrict;
@@ -111,27 +121,37 @@ alter table user_groups add constraint fk_user_groups_groups_02 foreign key (gro
 
 # --- !Downs
 
-SET FOREIGN_KEY_CHECKS=0;
+SET REFERENTIAL_INTEGRITY FALSE;
 
-drop table channel;
+drop table if exists channel;
 
-drop table channel_user;
+drop table if exists channel_user;
 
-drop table channel_groups;
+drop table if exists channel_groups;
 
-drop table channel_message;
+drop table if exists channel_message;
 
-drop table channel_file;
+drop table if exists channel_file;
 
-drop table file;
+drop table if exists file;
 
-drop table groups;
+drop table if exists groups;
 
-drop table user_groups;
+drop table if exists user_groups;
 
-drop table message;
+drop table if exists message;
 
-drop table user;
+drop table if exists user;
 
-SET FOREIGN_KEY_CHECKS=1;
+SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists channel_seq;
+
+drop sequence if exists file_seq;
+
+drop sequence if exists groups_seq;
+
+drop sequence if exists message_seq;
+
+drop sequence if exists user_seq;
 
