a248d13a80828c45ab054aa5e8e9170d2dddc4a4
MasterLindi <kridl.ovka1@gmx.at>
2012-05-26 22:02:22 +0200

    fixed new channel

--- src/webchat/app/websocket/json/in/InNewChannelData.java
+++ src/webchat/app/websocket/json/in/InNewChannelData.java
@@ -5,7 +5,5 @@ import java.util.*;
 public class InNewChannelData {
 	public String name;
 	public String topic;
-	public List<Integer> groups = new ArrayList<Integer>();
-	public List<Integer> users = new ArrayList<Integer>();
-	public Boolean priv;
+	public Boolean is_public;
 }
