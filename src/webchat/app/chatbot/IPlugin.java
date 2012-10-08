15b48cd2da677630bf5f9bbfede5326cc415b590
Daniel <danielherzog@gmx.at>
2012-10-08 16:47:31 +0200

    Chatbot base structure

---
+++ src/webchat/app/chatbot/IPlugin.java
@@ -0,0 +1,13 @@
+package chatbot;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Daniel
+ * Date: 08.10.12
+ * Time: 16:31
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IPlugin {
+    boolean check();
+    void execute();
+}
