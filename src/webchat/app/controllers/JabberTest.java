ce2b4c571b1df7d6bda8847b8aa622fb604a5bc2
Markus Gruber <if10b501@technikum-wien.at>
2012-11-27 14:28:22 +0100

    Added Multiuserchat in Jabber

--- src/webchat/app/controllers/JabberTest.java
+++ src/webchat/app/controllers/JabberTest.java
@@ -1,12 +1,26 @@
 package controllers;
 
-import org.jivesoftware.smack.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.jivesoftware.smack.Connection;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smackx.Form;
+import org.jivesoftware.smackx.FormField;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+
 import play.mvc.*;
-import play.mvc.Controller;
 
 public class JabberTest extends Controller {
 	
-	public static Result index() {
+	public static Result index() throws XMPPException {
 		
 		//Connect
 		ConnectionConfiguration config = new ConnectionConfiguration("localhost", 5222);
@@ -19,17 +33,52 @@ public class JabberTest extends Controller {
 		
 		//Login
 		try {
-			conn.login("markus", "markus");
+			conn.login("webchat", "test");
 		} catch (XMPPException e2) {
 		    System.out.println("Error logging in, Error: " + e2);
 		}
 		
+		//Create Chatroom
+		MultiUserChat muc = new MultiUserChat(conn,"testraum@conference.webchat");
+		muc.create("testraum");
+		
+		Form form = muc.getConfigurationForm();
+		Form submitForm = form.createAnswerForm();
+		
+	    for (Iterator fields = form.getFields(); fields.hasNext();) {
+	    	FormField field = (FormField) fields.next();
+	    	if (!FormField.TYPE_HIDDEN.equals(field.getType()) && field.getVariable() != null) {
+	    		submitForm.setDefaultAnswer(field.getVariable());
+	    	}
+	    }
+
+	    List owners = new ArrayList();
+	    owners.add("webchat@webchat");
+	    submitForm.setAnswer("muc#roomconfig_roomowners", owners);
+	    muc.sendConfigurationForm(submitForm);
+	    
+	    muc.invite("glembo@webchat/Smack", "testeinladung");
+	    
+	    muc.sendMessage("Servas burschn!");
+	    muc.addMessageListener(new PacketListener() {
+	    	@Override
+			public void processPacket(Packet packet) {
+				if (packet instanceof Message) {
+					System.out.println("Received message: " + (packet != null ? ((Message)packet).getFrom() + " " + ((Message)packet).getBody() : "NULL"));
+				} else if (packet instanceof Presence) {
+					System.out.println(packet);
+				}
+			}
+    	});
+	    
+	    /*
 		//Send Message
 		ChatManager cm = conn.getChatManager();	
 		Chat c = cm.createChat("glembo@webchat", new MessageListener() {
 			@Override
 			public void processMessage(Chat chat, org.jivesoftware.smack.packet.Message message) {
-		        System.out.println("Received message: " + message);
+				System.out.println("Received message: " + (message != null ? message.getBody() : "NULL"));
+				System.out.println(message);
 			}
 		});
 		try {
@@ -49,6 +98,7 @@ public class JabberTest extends Controller {
 					public void processMessage(Chat chat, org.jivesoftware.smack.packet.Message message)
 					{
 						System.out.println("Received message: " + (message != null ? message.getBody() : "NULL"));
+						System.out.println(message);
 					}
 				});
 		    }
@@ -88,6 +138,7 @@ public class JabberTest extends Controller {
 //		data.putAll(dat);
 //		injson.putObject("data").putAll(data);
 //		WebsocketNotifier.notifyAllMembers(m.genMessage(injson, 1));
+ */
 		
 		return ok();
     }
